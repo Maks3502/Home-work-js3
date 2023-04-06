de794eb75b455f278cd4023b4d0aae48f56890b3
Maks3502 <107855225+Maks3502@users.noreply.github.com>
2023-04-06 18:48:05 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const k1 = "5px" ;
+const k2 = "12djd334";
+const k3 = "12.45asdwe12";
+const k4 = "qwqweeewq";
+console.log(Number.parseInt(k1));
+console.log(Number.parseInt(k2));
+console.log(Number.parseInt(k3));
+console.log(Number.parseInt(k4));
+// 2-завдання
+const k5 = "5px";
+const k6 = "12djd334";
+const k7 = "12.45asdwe12";
+const k8  = "qwqweeewq";
+console.log(Number.parseFloat(k5));
+console.log(Number.parseFloat(k6)); 
+console.log(Number.parseFloat(k7)); 
+console.log(Number.parseFloat(k8));
+// 3-завдання
+// let maxNum = (733,99,36);
+// let minNum = (3,18,22);
+// let num = Math.random() * (maxNum - minNum) + minNum;
+// console.log(num);
+// num = Math.round(num);
+const bigestNum = (733,99,36);
+const smallerNum = (3,18,22);
+console.log(bigestNum);
+console.log(smallerNum);
+// 4-завдання
+let maxNum = 3;  
+let minNum = 19;  
+let num = Math.random() * (maxNum - minNum) + minNum;  
+console.log(num);  
+num = Math.round(num);
